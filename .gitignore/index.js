99d8ee0f0fb6c867101439cd82f40702eb1c9fca
sysysysy242 <45331186+sysysysy242@users.noreply.github.com>
2019-09-28 15:48:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -37,4 +37,4 @@ client.on('message', message => {
 
 
 
-client.login("NjI3NDkwODI3NzQ4NTA3NjQ4.XY9hHQ.dsXXOcJp0OJBFuZ8EzUkaLDJZhc")
+client.login("NjIzOTEwNTAzMTEwMDE3MDQx.XY9Bww.nXkzkZ5h8_VWV8vAsjVdWm9m3W4")
